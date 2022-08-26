8b9d6e1593c7f5fc5f88cf33c1f8e31a1021d926
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-08-26 13:24:45 -0700

    wip determe price/cost formulat

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,17 @@
 function Order () {
   this.pizzas = {};
   this.currentId = 0;
-  this.balance = parseInt(balance);
 }
-
-Order.prototype.addPIzza = fucntion (pizza) {
-  pizza.id = this.assighnId();
-  this.pizza[pizza.id] = pizza;
-};
 
 Order.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId
 }
+
+Order.prototype.addPizza = function(pizza) {
+  pizza.id = this.assignId();
+  this.pizzas[pizza.id] = pizza;
+};
 
 Order.prototype.findPizza = function(id) {
   if (this.pizza[id] !== undefined) {
@@ -25,4 +24,8 @@ Order.prototype.findPizza = function(id) {
 function Pizza (pizzaToppings, size) {
 this.toppings = pizzaToppings;
 this.size = size
+}
+
+Pizza.prototype.price = function() {
+  
 }
